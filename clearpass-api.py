01212079645175620d718b4820a335b897d9e5a9
Robert Schneider <robmschn@gmail.com>
2019-11-27 09:10:02 -0500

    initial upload

--- clearpass-api.py
+++ clearpass-api.py
@@ -2,4 +2,51 @@
 
 # Requires Python3 and requests
 
-#test upload
+# imports
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+from urllib3 import disable_warnings
+
+# import creds
+import sys
+import creds
+
+# change file path to use creds.py
+sys.path.append("..")
+
+# definitions
+username = creds.username
+password = creds.password
+ip_address = creds.ip_address
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(username, password)
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    url = 'https://' + ip_address + '/ins'
+
+    payload = {
+        "ins_api": {
+            "version": "1.0",
+            "type": "cli_show",
+            "chunk": "0",
+            "sid": "1",
+            "input": "sh ver",
+            "output_format": "json"
+        }
+    }
+
+    disable_warnings()
+    response = requests.post(url, data=json.dumps(payload), headers=headers, auth=auth, verify=False)
+
+    result = json.loads(response.text)
+
+    # isolate just the body tag
+    result_body = result['ins_api']['outputs']['output']['body']
+
+    print(result_body['host_name'])
